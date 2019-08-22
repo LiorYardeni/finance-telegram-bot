4dc5c5dad5a2f74d4ade8658833923f4e6c91f2c
lioryardeni <liorwhy@gmail.com>
2019-08-22 18:56:41 +0300

    first commit

--- FinanceWeb/finance_web/urls.py
+++ FinanceWeb/finance_web/urls.py
@@ -1,4 +1,4 @@
-"""finance_django URL Configuration
+"""finance_web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- FinanceWeb/finance_web/wsgi.py
+++ FinanceWeb/finance_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for finance_django project.
+WSGI config for finance_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_web.settings')
 
 application = get_wsgi_application()
